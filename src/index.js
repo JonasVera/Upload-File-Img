1a8e4ae3e50b94641a29a7c9b1330ee2e6f008d1
Jonas Silva <jonasverasilva@gmail.com>
2020-09-24 16:49:53 -0300

    Ajuste do mongo

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
  
-mongoose.connect(process.env.MONGO_URL,{
+mongoose.connect(process.env.MONGO_URI,{
     useNewUrlParser:true,
-  
+    useUnifiedTopology:true
 });
 
 app.use(cors());
